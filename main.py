511a31b1cc83142c5303155532136dc966b08d9d
Mohamed Massinissa, Ziane <e2185817@cmaisonneuve.qc.ca>
2026-03-17 20:15:45 -0400

    feat: implement get_transactions with date range filtering

--- main.py
+++ main.py
@@ -3,9 +3,11 @@ import csv
 from datetime import datetime
 from data_entry import get_date, get_amount, get_category, get_description
 
+
 class CSV:
     CSV_FILE = 'finance_data.csv'
     COLUMNS = ["date", "amount", "category", "description"]
+    FORMAT = "%d-%m-%Y"
 
     @classmethod
     def initialize_csv(cls):
@@ -34,6 +36,40 @@ class CSV:
             writer.writerow(new_entry)
         print("Entry added successfully!")
 
+    @classmethod
+    def get_transactions(cls, start_date, end_date):
+        df = pd.read_csv(cls.CSV_FILE)
+        # Convert the date column to datetime format for filtering
+        df['date'] = pd.to_datetime(df['date'], format=CSV.FORMAT)
+        start_date = datetime.strptime(start_date, CSV.FORMAT)
+        end_date = datetime.strptime(end_date, CSV.FORMAT)
+        
+        # Filter the DataFrame based on the date range
+        mask = (df['date'] >= start_date) & (df['date'] <= end_date)
+        filtered_df = df.loc[mask]
+
+        if filtered_df.empty:
+            print("No transactions found in the specified date range.")
+        else:
+            print(
+                f"Transactions from {start_date.strftime(CSV.FORMAT)} to {end_date.strftime(CSV.FORMAT)}:"
+                )
+            print(
+                filtered_df.to_string(
+                    index=False, formatters={'date': lambda x: x.strftime(CSV.FORMAT)}
+                    )
+                )
+            
+            total_income = filtered_df[filtered_df['category'] == 'Income']['amount'].sum()
+            total_expense = filtered_df[filtered_df['category'] == 'Expense']['amount'].sum()
+            print("\nSummary:")
+            print(f"Total Income: ${total_income:.2f}")
+            print(f"Total Expense: ${total_expense:.2f}")
+            print(f"Net Savings: ${(total_income - total_expense):.2f}")
+
+        return filtered_df
+
+
 # data collection function from the input helper functions
 def add():
     CSV.initialize_csv()
@@ -48,4 +84,5 @@ def add():
 
 
 # Test
+CSV.get_transactions("01-08-2023", "17-03-2026")
 add()
